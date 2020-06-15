0f65b1b322377478e680550cf7987e19ea5af498
Dominik Schidlowski <dominik@schidlowski.eu>
2020-06-15 18:42:55 +0200

    add viper configer

--- lib/settings.go
+++ lib/settings.go
@@ -1,11 +1,15 @@
 package lib
 
-import "os/user"
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"os/user"
+)
 
 type Settings struct {
 	ConversionDestinationDirectory string
-	UploadDestinationDirectory string
-	HttpListenEndpoint string
+	UploadDestinationDirectory     string
+	HttpListenEndpoint             string
 }
 
 var settings *Settings = initSettings()
@@ -16,10 +20,29 @@ func initSettings() *Settings {
 		panic(err)
 	}
 
+	loadConfigFile(currentUser.HomeDir)
+
 	return &Settings{
-		ConversionDestinationDirectory: currentUser.HomeDir,
-		UploadDestinationDirectory: currentUser.HomeDir,
-		HttpListenEndpoint: "0.0.0.0:8080",
+		ConversionDestinationDirectory: viper.GetString("ConversionDestinationDirectory"),
+		UploadDestinationDirectory:     viper.GetString("UploadDestinationDirectory"),
+		HttpListenEndpoint:             viper.GetString("HttpListenEndpoint"),
+	}
+}
+
+func loadConfigFile(homePath string) {
+	viper.SetDefault("ConversionDestinationDirectory", homePath)
+	viper.SetDefault("UploadDestinationDirectory", homePath)
+	viper.SetDefault("HttpListenEndpoint", "0.0.0.0:8080")
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("/etc/ffmpeg-server/")
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return
+		}
+
+		logrus.Errorln(err)
 	}
 }
 
